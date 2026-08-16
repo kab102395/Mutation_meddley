4365e2b650c7cb3eda1cc2823c3842e630e5c955
Kyle A. Barrett <100321645+kab102395@users.noreply.github.com>
2026-08-15 21:02:02 -0700

    Remove duplicate Serializable from Biology menu partial

--- Code/MutationMeddley_BiologySupport.CoreB.cs
+++ Code/MutationMeddley_BiologySupport.CoreB.cs
@@ -10,7 +10,6 @@ using XRL.World.Parts.Mutation;
 
 namespace XRL.World.Parts
 {
-    [Serializable]
     public partial class MutationMeddley_BiologySupport : IPart
     {
         private string MutationMeddley_GetActionSignature(MutationMeddley_AdaptiveMutationBase mutation)
