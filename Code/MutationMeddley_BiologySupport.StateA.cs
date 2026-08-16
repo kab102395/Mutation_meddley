fc9d9f9cf3a451eed947297f9c160702b4d29de8
Kyle A. Barrett <100321645+kab102395@users.noreply.github.com>
2026-08-15 21:00:40 -0700

    Remove duplicate Serializable from Biology state partial

--- Code/MutationMeddley_BiologySupport.StateA.cs
+++ Code/MutationMeddley_BiologySupport.StateA.cs
@@ -10,7 +10,6 @@ using XRL.World.Parts.Mutation;
 
 namespace XRL.World.Parts
 {
-    [Serializable]
     public partial class MutationMeddley_BiologySupport : IPart
     {
         private bool MutationMeddley_HasAnyEvolution(MutationMeddley_AdaptiveMutationBase mutation)
