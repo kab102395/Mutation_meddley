e561f54472ba614c05a112be1ee90f8b8d206c22
Kyle A. Barrett <100321645+kab102395@users.noreply.github.com>
2026-08-16 19:43:44 -0700

    Remove dead support-owned action handlers

--- Code/MutationMeddley_BiologySupport.ActionsB.cs
+++ Code/MutationMeddley_BiologySupport.ActionsB.cs
@@ -1,10 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Text;
-using XRL;
-using XRL.Core;
-using XRL.Messages;
 using XRL.UI;
 using XRL.World.Parts.Mutation;
 
@@ -12,47 +7,6 @@ namespace XRL.World.Parts
 {
     public partial class MutationMeddley_BiologySupport : IPart
     {
-        private string MutationMeddley_GetHighestAttunementKey(MutationMeddley_AdaptiveMutationBase mutation)
-        {
-            int heat = MutationMeddley_GetStateInt(mutation, "carapace_attune_heat");
-            int mire = MutationMeddley_GetStateInt(mutation, "carapace_attune_mire");
-            int rime = MutationMeddley_GetStateInt(mutation, "carapace_attune_rime");
-
-            if (heat <= 0 && mire <= 0 && rime <= 0)
-            {
-                return "";
-            }
-
-            if (heat >= mire && heat >= rime) return "carapace_attune_heat";
-            if (mire >= rime) return "carapace_attune_mire";
-            return "carapace_attune_rime";
-        }
-
-        private void MutationMeddley_UseCarapaceAction()
-        {
-            MutationMeddley_UsePrimaryAction(MutationMeddley_GetMutation("Carapace Evolution"));
-        }
-
-        private void MutationMeddley_UseCrystalAction()
-        {
-            MutationMeddley_UsePrimaryAction(MutationMeddley_GetMutation("Living Crystal"));
-        }
-
-        private void MutationMeddley_UseBrineAction()
-        {
-            MutationMeddley_UsePrimaryAction(MutationMeddley_GetMutation("Brineborn"));
-        }
-
-        private void MutationMeddley_UseAshAction()
-        {
-            MutationMeddley_UsePrimaryAction(MutationMeddley_GetMutation("Ash Metabolism"));
-        }
-
-        private void MutationMeddley_UseColonyAction()
-        {
-            MutationMeddley_UsePrimaryAction(MutationMeddley_GetMutation("Walking Colony"));
-        }
-
         private void MutationMeddley_ShowAllModifiers(List<MutationMeddley_AdaptiveMutationBase> owned)
         {
             StringBuilder text = new StringBuilder("CURRENT MUTATION MEDDLEY MODIFIERS\n");
@@ -95,6 +49,5 @@ namespace XRL.World.Parts
             }
             Popup.Show(text.ToString());
         }
-
     }
 }
